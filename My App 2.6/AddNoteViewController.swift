243dc1dc9f6c40b5f78cec6300ebcb9737793318
Sean Doyle <sdoylelambda@gmail.com>
2019-02-06 12:24:57 -0500

    View Controllers added.

---
+++ My App 2.6/AddNoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNoteViewController.swift
+//  My App 2.6
+//
+//  Created by Lambda_School_Loaner_18 on 2/6/19.
+//  Copyright © 2019 SeanDoyle. All rights reserved.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
