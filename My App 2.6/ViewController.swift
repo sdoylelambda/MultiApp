e69ee458d8e568e928dfb8cc7ffc8dfc56091cd3
Sean Doyle <sdoylelambda@gmail.com>
2019-02-06 11:50:21 -0500

    Basic storyboard, VC for theme selection, TableVC for note pad.:

---
+++ My App 2.6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My App 2.6
+//
+//  Created by Lambda_School_Loaner_18 on 2/6/19.
+//  Copyright © 2019 SeanDoyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
